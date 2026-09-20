16315f03c05cd53cc35e953f7945c5909a8680d0
todoom1 <aa13976383@outlook.com>
2026-09-20 19:35:48 +0800

    Update list.js

--- list.js
+++ list.js
@@ -1,5 +1,6 @@
 (function() {
     var secretApps = [
+{img: './png/xha.jpg', name: '西海岸', xurl: 'aHR0cHM6Ly9tb2ZmeW0uaGNqd2ouY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0yMDkwJnB0PTM1MzBEMEMwLTRDN0EtMEM4Mi00RTZCLTVBQzM4MTc4MzU4RA=='}, 
 {img: './png/zhgj.jpg', name: '纵横国际', xurl: 'aHR0cDovL3hucXd6Zi50Y2txeHguY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDY3JnRvcElkPTI2OTE3ODUxNjk0OTc3Nzkmc2VsZlBsYW5JZD0yNTczNw=='},
 {img: './png/xjh.jpg', name: '聚鑫汇', xurl: 'aHR0cHM6Ly95Ymt2YnoudGNrcXh4LmNvbS9hcHAvcmVnaXN0ZXIucGhwP3NpdGVfaWQ9MTA2NiZ0b3BJZD0yNjQ5Mzcmc2VsZlBsYW5JZD0yNjY3NzU='},
 {img: './png/jlgj.jpg', name: '君临国际', xurl: 'aHR0cHM6Ly90ZmFqbi5oY2p3ai5jb20vYXBwL3JlZ2lzdGVyLnBocD9zaXRlX2lkPTIwODEmdG9wSWQ9MjM4MTA1NyZzZWxmUGxhbklkPTIxMTM0NDg='},
